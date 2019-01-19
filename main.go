0f240e4f40023ae49828066dc41ee6ffb8794ffd
jiro4989 <jiroron666@gmail.com>
2019-01-20 05:29:18 +0900

    :sparkles:出力設定を定義

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 type (
 	WindowColor    int
 	WindowPosition int
+	ActorNameColor int
 )
 
 //go:generate stringer -type=WindowColor
@@ -29,9 +30,21 @@ const (
 
 type Message struct {
 	ActorName       string
-	Texts           []string
+	Body            []string
 	BackgroundColor int
 	WindowPosition  int
+}
+
+// Settings はMapXXXJSON出力設定
+// See https://qiita.com/aosho235/items/ef7a99396f69442d2cf2
+type Settings struct {
+	ActorNameBracketStart string // アクター名を括る括弧(開き)
+	ActorNameBracketEnd   string // アクター名を括る括弧(閉じ)
+	BodyBracketStart      string // 本文を括る括弧(開き)
+	BodyBracketEnd        string // 本文を括る括弧(閉じ)
+	Indent                string // インデント
+	BracketAlign          bool   // 括弧の位置に揃えるか否か
+	ActorNameColor        int    // アクター名につける色
 }
 
 // /Hello/:langにハンドルされているHello関数
