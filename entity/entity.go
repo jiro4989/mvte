3ae6bf4379a01cd0996b5dab5746bcc06abd7a06
jiro4989 <jiroron666@gmail.com>
2019-01-20 11:42:13 +0900

    :construction:パッケージを分けた

---
+++ entity/entity.go
@@ -0,0 +1,38 @@
+package entity
+
+type (
+	WindowColor    int
+	WindowPosition int
+	ActorNameColor int
+)
+
+//go:generate stringer -type=WindowColor
+//go:generate stringer -type=WindowPosition
+
+const (
+	WCWindow WindowColor = iota
+	WCDark
+	WCTransparent
+	WPTop WindowPosition = iota
+	WPMiddle
+	WPBottom
+)
+
+type Message struct {
+	ActorName      string
+	Body           []string
+	WindowColor    WindowColor
+	WindowPosition WindowPosition
+}
+
+// Settings はMapXXXJSON出力設定
+// See https://qiita.com/aosho235/items/ef7a99396f69442d2cf2
+type Settings struct {
+	ActorNameBracketStart string         // アクター名を括る括弧(開き)
+	ActorNameBracketEnd   string         // アクター名を括る括弧(閉じ)
+	BodyBracketStart      string         // 本文を括る括弧(開き)
+	BodyBracketEnd        string         // 本文を括る括弧(閉じ)
+	Indent                string         // インデント
+	BracketAlign          bool           // 括弧の位置に揃えるか否か
+	ActorNameColor        ActorNameColor // アクター名につける色
+}
--- main.go
+++ main.go
@@ -10,43 +10,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type (
-	WindowColor    int
-	WindowPosition int
-	ActorNameColor int
-)
-
-//go:generate stringer -type=WindowColor
-//go:generate stringer -type=WindowPosition
-
-const (
-	WCWindow WindowColor = iota
-	WCDark
-	WCTransparent
-	WPTop WindowPosition = iota
-	WPMiddle
-	WPBottom
-)
-
-type Message struct {
-	ActorName      string
-	Body           []string
-	WindowColor    WindowColor
-	WindowPosition WindowPosition
-}
-
-// Settings はMapXXXJSON出力設定
-// See https://qiita.com/aosho235/items/ef7a99396f69442d2cf2
-type Settings struct {
-	ActorNameBracketStart string         // アクター名を括る括弧(開き)
-	ActorNameBracketEnd   string         // アクター名を括る括弧(閉じ)
-	BodyBracketStart      string         // 本文を括る括弧(開き)
-	BodyBracketEnd        string         // 本文を括る括弧(閉じ)
-	Indent                string         // インデント
-	BracketAlign          bool           // 括弧の位置に揃えるか否か
-	ActorNameColor        ActorNameColor // アクター名につける色
-}
-
 // /Hello/:langにハンドルされているHello関数
 func Hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	lang := p.ByName("lang") // langパラメーターを取得する
